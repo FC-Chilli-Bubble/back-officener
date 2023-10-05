8db24b20bea8fee6fd67cfcc1cd51421b9c2164a
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-05 23:56:03 +0900

    fix #221: Redis 설정 변경
    - 사용하지 않을 StringTemplate 삭제
    - Object를 직렬화할 수 있도록 Jackson 사용

--- src/main/java/fastcampus/team7/Livable_officener/global/config/RedisRepositoryConfig.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/config/RedisRepositoryConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -29,14 +29,9 @@ public class RedisRepositoryConfig {
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setKeySerializer(StringRedisSerializer.UTF_8);
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return redisTemplate;
-    }
-
-    @Bean
-    public StringRedisTemplate stringRedisTemplate() {
-        return new StringRedisTemplate(redisConnectionFactory());
     }
 }
