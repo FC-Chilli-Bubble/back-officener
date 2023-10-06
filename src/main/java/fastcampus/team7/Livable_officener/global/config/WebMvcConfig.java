10273f086f16b9d04ce830e57d2924ceebc67261
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-06 12:59:19 +0900

    chore #226: 요청마다 로깅 설정

---
+++ src/main/java/fastcampus/team7/Livable_officener/global/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package fastcampus.team7.Livable_officener.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import fastcampus.team7.Livable_officener.global.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/images/**");
+    }
+}
+
