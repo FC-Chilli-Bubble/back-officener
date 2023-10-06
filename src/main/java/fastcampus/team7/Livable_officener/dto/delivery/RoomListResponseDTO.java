69e1ced4216d1a27aab54a1489455693718a8d2f
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-06 13:04:18 +0900

    chore #226: 함께배달 리스트 deadline `@JsonFormat` 설정

--- src/main/java/fastcampus/team7/Livable_officener/dto/delivery/RoomListResponseDTO.java
+++ src/main/java/fastcampus/team7/Livable_officener/dto/delivery/RoomListResponseDTO.java
@@ -1,5 +1,6 @@
 package fastcampus.team7.Livable_officener.dto.delivery;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class RoomListResponseDTO {
     private String tag;
     private Long attendees;
     private Long maxAttendees;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime deadLine;
     private String roomStatus;
     private LocalDateTime createdAt;
