ff884a892e94f2dd6f078b178e414d0bc9ec0450
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-06 14:24:31 +0900

    chore #228: 예외 상속 구조 변경

--- src/main/java/fastcampus/team7/Livable_officener/global/exception/AlreadyLogoutException.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/exception/AlreadyLogoutException.java
@@ -2,7 +2,7 @@ package fastcampus.team7.Livable_officener.global.exception;
 
 import fastcampus.team7.Livable_officener.global.constant.FilterExceptionCode;
 
-public class AlreadyLogoutException extends RuntimeException {
+public class AlreadyLogoutException extends IllegalStateException {
     public AlreadyLogoutException(FilterExceptionCode filterExceptionCode) {
         super(filterExceptionCode.getMessage());
     }
--- src/main/java/fastcampus/team7/Livable_officener/global/exception/NotFoundUserException.java
+++ src/main/java/fastcampus/team7/Livable_officener/global/exception/NotFoundUserException.java
@@ -1,6 +1,6 @@
 package fastcampus.team7.Livable_officener.global.exception;
 
-public class NotFoundUserException extends RuntimeException{
+public class NotFoundUserException extends IllegalArgumentException {
 
     public static final String DEFAULT_MESSAGE = "존재하지 않는 회원입니다.";
 
