87a608fce04d17f60b186ab831ce0a2754757a57
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-06 12:49:26 +0900

    fix #226: redis Object 변환 에러

--- src/main/java/fastcampus/team7/Livable_officener/dto/fcm/FCMStatusDTO.java
+++ src/main/java/fastcampus/team7/Livable_officener/dto/fcm/FCMStatusDTO.java
@@ -3,10 +3,12 @@ package fastcampus.team7.Livable_officener.dto.fcm;
 import fastcampus.team7.Livable_officener.global.constant.FCMNotificationStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class FCMStatusDTO {
     private String fcmToken;
